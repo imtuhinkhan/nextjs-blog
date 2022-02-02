7916dc9aa4b9e39e2656bc3d8bb75cbddd0121f1
Tuhin Khan <uiu.tuhin@gmail.com>
2022-02-02 22:33:11 +0600

    added footer

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,6 @@ export default function Main() {
       <main className={styles.main}>
         <a href='/home'>Home Page</a>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Tuhin
-        </a>
       </footer>
     </div>
   )
