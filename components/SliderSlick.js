195ad8006b8a9fe4b308d38b69c5c914d28cc040
Tuhin Khan <uiu.tuhin@gmail.com>
2022-02-10 00:07:54 +0600

    clean code

--- components/SliderSlick.js
+++ components/SliderSlick.js
@@ -3,16 +3,6 @@ import { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import Slider from "react-slick";
 const SliderSlick = () => {
-  const [nav1, setNav1] = useState();
-  const [nav2, setNav2] = useState();
-  const slider1 = useRef();
-  const slider2 = useRef();
-
-  // i dont seem to need this
-  //useEffect(() => {
-  //    setNav1(sliderEl1);
-  //    setNav2(sliderEl2);
-  //  }, []);
 
   return (
       <div class="section-welcome">
